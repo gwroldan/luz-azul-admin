93d60554200c73063d9a2fd23d124a35857d18ba
gwroldan <gustavowroldan@gmail.com>
2018-04-01 13:19:15 -0300

    Add Stepper

--- src/app/pedidos/agrupar/agrupar-pedidos.component.ts
+++ src/app/pedidos/agrupar/agrupar-pedidos.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 import * as XLSX from 'xlsx';
 import swal from 'sweetalert2';
+import { NgxStepperComponent, StepperOptions } from 'ngx-stepper';
 
 type AOA = any[][];
 
@@ -10,17 +13,40 @@ type AOA = any[][];
   templateUrl: './agrupar-pedidos.component.html',
   styleUrls: ['./agrupar-pedidos.component.scss']
 })
-export class AgruparPedidosComponent {
+export class AgruparPedidosComponent implements OnInit {
   public inputFileModel: Array<any> = new Array<any>();
-  public inputMaxFiles: number = 20;
+  public inputMaxFiles: number = 40;
   public inputAccept: string = '.xls,.xlsx';
+  public data: AOA;
   private cabecera: AOA = [
       ['Empresa:', 'Ensemble SRL'],
       ['Fecha:', new Date()],
       [],
       ['Cod. Producto', 'Descripcion', 'Cant. Pedida', 'Cant. Real', 'Kg. Reales', 'Kg. Pedidos', 'Lote']
     ];
+  private internalFileModel: Array<any> = new Array<any>(); // I need for disconetion of model when remove all files
   private fileName = 'PedidoAgrupado.xls';
+
+  @ViewChild('stepperDemo')
+  public steppers: NgxStepperComponent;
+  public options: StepperOptions = {
+    vertical: false,
+    linear: true,
+    alternative: true,
+    mobileStepText: false,
+    enableSvgIcon: true,
+    labelStep: 'Paso',
+    labelOf: 'de'
+  };
+
+  constructor(private _iconRegistry: MatIconRegistry, private _sanitizer: DomSanitizer) { }
+
+  public ngOnInit(): void {
+    this._iconRegistry
+      .addSvgIcon('step-done', this._sanitizer.bypassSecurityTrustResourceUrl('assets/icon/done.svg'));
+    this._iconRegistry
+      .addSvgIcon('step-warning', this._sanitizer.bypassSecurityTrustResourceUrl('assets/icon/warning.svg'));
+  }
 
   private readMultipleFiles = (files) => {
     let detalle: AOA;
@@ -102,65 +128,31 @@ export class AgruparPedidosComponent {
     return detAgrupado;
   }
 
-  private exportFile(data) {
-    /* generate worksheet */
-    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
-
-    /* generate workbook and add the worksheet */
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-    /* save to file */
-    XLSX.writeFile(wb, this.fileName);
-  }
-
-  public onAccept(file: any): void {
-    console.log('accept');
-  }
-
-  public onRemove(file: any): void {
-    console.log('remove');
-  }
-
-  public onLimit(file: any): void {
-    console.log('limit');
-    swal({
-      type: 'info',
-      title: 'Limite',
-      text: `Solo se pueden cargar hasta ${this.inputMaxFiles} archivos!`
-    });
-  }
-
-  public onReject(file: any): void {
-    console.log('reject');
-    swal({
-      type: 'warning',
-      title: 'Archivo Invalido',
-      html: `Solo se pueden cargar archivos con extension <b>${this.inputAccept}</b>!`
-    });
-  }
-
-  public generar(): void {
+  private exportFile() {
     swal({
       title: 'Esta Seguro?',
-      text: 'Desea agrupar los pedidos seleccionados?',
+      text: 'Desea exportar los datos seleccionados?',
       type: 'question',
       showCancelButton: true,
-      confirmButtonText: 'Agrupar',
+      confirmButtonText: 'Exportar',
       cancelButtonText: 'Cancelar',
       showLoaderOnConfirm: true,
       preConfirm: () => {
         return new Promise((resolve) => {
+          this.steppers.next();
+
           setTimeout(() => {
-            this.readMultipleFiles(this.inputFileModel)
-              .then((detalle: AOA) => {
-                this.exportFile([ ...this.cabecera, ...this.agruparDetalle(detalle)]);
-              })
-              .catch((err) => {
-                console.error('Ocurrio un error inesperado:', err);
-              });
-            resolve();
-          }, 1000);
+              /* generate worksheet */
+              const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+              /* generate workbook and add the worksheet */
+              const wb: XLSX.WorkBook = XLSX.utils.book_new();
+              XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+              /* save to file */
+              XLSX.writeFile(wb, this.fileName);
+              resolve();
+            }, 1000);
         });
       },
       allowOutsideClick: () => !swal.isLoading()
@@ -175,5 +167,54 @@ export class AgruparPedidosComponent {
         });
       }
     });
+  }
+
+  public onAccept(file: any): void {
+    console.log('accept');
+    this.internalFileModel.push(file);
+  }
+
+  public onRemove(file: any): void {
+    console.log('remove');
+    this.internalFileModel.splice(this.internalFileModel.indexOf(file), 1);
+  }
+
+  public onLimit(): void {
+    console.log('limit');
+    swal({
+      type: 'info',
+      title: 'Limite',
+      text: `Solo se pueden cargar hasta ${this.inputMaxFiles} archivos!`
+    });
+  }
+
+  public onReject(): void {
+    console.log('reject');
+    swal({
+      type: 'warning',
+      title: 'Archivo Invalido',
+      html: `Solo se pueden cargar archivos con extension <b>${this.inputAccept}</b>!`
+    });
+  }
+
+  public previousStep(): void {
+    this.steppers.back();
+  }
+
+  public nextStep(): void {
+    if (this.internalFileModel.length > 1) {
+      this.steppers.clearError();
+
+      this.readMultipleFiles(this.internalFileModel)
+        .then((detalle: AOA) => {
+          this.data = [ ...this.cabecera, ...this.agruparDetalle(detalle)];
+          this.steppers.next();
+        })
+        .catch((err) => {
+          console.error('Ocurrio un error inesperado:', err);
+        });
+    } else {
+      this.steppers.error('Debe seleccionar al menos 2 pedidos');
+    }
   }
 }
